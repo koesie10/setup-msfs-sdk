25eceeea4cbd69fbd115ef8c6fcdaad728b1bf6c
Koen Vlaswinkel <koen@vlaswinkel.info>
2022-10-22 11:58:21 +0200

    Add caching of SDK

--- dist/index.js
+++ dist/index.js
@@ -43,17 +43,35 @@ const core = __importStar(__nccwpck_require__(2186));
 const fs = __importStar(__nccwpck_require__(7147));
 const path = __importStar(__nccwpck_require__(1017));
 const tc = __importStar(__nccwpck_require__(7784));
+const versionRegex = /\b(\d+\.\d+\.\d+\.\d+)\b/;
+const toolName = 'msfs-sdk';
 function run() {
+    var _a, _b;
     return __awaiter(this, void 0, void 0, function* () {
         try {
             const url = core.getInput('url');
-            const msfsSdkZipPath = yield tc.downloadTool(url);
-            const msfsSdkPath = yield tc.extractZip(msfsSdkZipPath);
-            const version = fs
-                .readFileSync(path.join(msfsSdkPath, 'version.txt'), 'utf8')
-                .trim();
-            const cachedPath = yield tc.cacheDir(msfsSdkPath, 'msfs-sdk', version);
-            core.exportVariable('MSFS_SDK', cachedPath);
+            let toolPath;
+            const version = (_b = (_a = versionRegex.exec(url)) === null || _a === void 0 ? void 0 : _a.groups) === null || _b === void 0 ? void 0 : _b[1];
+            if (version) {
+                toolPath = tc.find(toolName, version);
+                core.info(`Found version ${version} in cache`);
+            }
+            if (!toolPath) {
+                core.info(`Attempting to download SDK`);
+                const msfsSdkZipPath = yield tc.downloadTool(url);
+                core.info(`Extracting SDK`);
+                const msfsSdkPath = yield tc.extractZip(msfsSdkZipPath);
+                core.info(`Reading version from SDK`);
+                const downloadedVersion = fs
+                    .readFileSync(path.join(msfsSdkPath, 'version.txt'), 'utf8')
+                    .trim();
+                if (version && version !== downloadedVersion) {
+                    core.warning(`Version mismatch: expected ${version}, got ${downloadedVersion}`);
+                }
+                core.info(`Caching SDK version ${version}`);
+                toolPath = yield tc.cacheDir(msfsSdkPath, toolName, downloadedVersion);
+            }
+            core.exportVariable('MSFS_SDK', toolPath);
         }
         catch (error) {
             if (error instanceof Error)
--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,48 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as tc from '@actions/tool-cache';
 
+const versionRegex = /\b(\d+\.\d+\.\d+\.\d+)\b/;
+const toolName = 'msfs-sdk';
+
 async function run(): Promise<void> {
   try {
     const url: string = core.getInput('url');
 
-    const msfsSdkZipPath = await tc.downloadTool(url);
-    const msfsSdkPath = await tc.extractZip(msfsSdkZipPath);
+    let toolPath: string | undefined;
 
-    const version = fs
-      .readFileSync(path.join(msfsSdkPath, 'version.txt'), 'utf8')
-      .trim();
+    const version = versionRegex.exec(url)?.groups?.[1];
+    if (version) {
+      toolPath = tc.find(toolName, version);
+      core.info(`Found version ${version} in cache`);
+    }
 
-    const cachedPath = await tc.cacheDir(msfsSdkPath, 'msfs-sdk', version);
+    if (!toolPath) {
+      core.info(`Attempting to download SDK`);
 
-    core.exportVariable('MSFS_SDK', cachedPath);
+      const msfsSdkZipPath = await tc.downloadTool(url);
+
+      core.info(`Extracting SDK`);
+
+      const msfsSdkPath = await tc.extractZip(msfsSdkZipPath);
+
+      core.info(`Reading version from SDK`);
+
+      const downloadedVersion = fs
+        .readFileSync(path.join(msfsSdkPath, 'version.txt'), 'utf8')
+        .trim();
+
+      if (version && version !== downloadedVersion) {
+        core.warning(
+          `Version mismatch: expected ${version}, got ${downloadedVersion}`
+        );
+      }
+
+      core.info(`Caching SDK version ${version}`);
+
+      toolPath = await tc.cacheDir(msfsSdkPath, toolName, downloadedVersion);
+    }
+
+    core.exportVariable('MSFS_SDK', toolPath);
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message);
   }
